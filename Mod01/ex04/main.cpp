eeb8e26faae9257eea7d95eede1670726e854f6b
hmontoya <hmontoya@student.42barcelona.com>
2024-10-23 19:04:30 +0200

    open file protections

--- Mod01/ex04/main.cpp
+++ Mod01/ex04/main.cpp
@@ -7,42 +7,54 @@
 //2. read line
 //3. find ocurrence in line or go copy line in file and go to step 2
 //4. if occurence in line 
-	//4.1 copy line until occurence in replace file
-	//4.2 copy replacement string into replace file
+	//4.1 copy line until occurence in outputace file
+	//4.2 copy outputacement string into outputace file
 //5  else
 	//5.1 copy line
 	//go to step 3 
 
 /**
  * @brief It searches occurences of a sting into a file and create a new
- * file with the occurrences replaced with the string given. 
+ * file with the occurrences outputaced with the string given. 
 */
-void replacefile(std::string fread, std::string freplace, std::string socurr, std::string sreplace)
+void outputacefile(std::string fread, std::string foutputace, std::string socurr, std::string soutputace)
 {
-	std::ifstream	toread(fread.c_str());
-	std::ofstream	repl(freplace.c_str());
+	std::ifstream	input(fread.c_str());
+	if (!input.is_open())
+	{
+		std::cout << "> Error opening input file" << std::endl;
+		return ;
+	}
+	std::ofstream	output(foutputace.c_str());
+	if (!output.is_open())
+	{
+		std::cout << "> Error opening output file" << std::endl;
+		return ;
+	}
 	std::string		line;
 	size_t			occur;
 
-	while (std::getline(toread, line))
+	while (std::getline(input, line))
 	{
 		occur = line.find(socurr);
 		if (occur == std::string::npos)
 		{
-			if (repl.is_open())
-				repl << line << std::endl;
+			if (output.is_open())
+				output << line << std::endl;
 			continue ;
 		}
-		repl << line.substr(0, occur) << sreplace << line.substr(occur + socurr.length(), line.length()) << std::endl;
+		output << line.substr(0, occur) << soutputace << line.substr(occur + socurr.length(), line.length()) << std::endl;
 	}
+	input.close();
+	output.close();
 }
 
 /**
- * @brief It replace a given content on a given file
- * with the given string to replace with.
+ * @brief It outputace a given content on a given file
+ * with the given string to outputace with.
  * @param filename `{string}` the filename to read
  * @param s1 `{string}` the occurence in the file
- * @param s2 `{sting}` the string to replace with 
+ * @param s2 `{sting}` the string to outputace with 
  * @returns `{void}`
 */
 int main(int ac, char **av)
@@ -50,9 +62,9 @@ int main(int ac, char **av)
 	if (ac != 4)	
 	{
 		std::cout << "Wrong number of arguments" << std::endl;
-		std::cout << "USAGE: ./powersed [filename] [occurence] [replacement]" << std::endl;
+		std::cout << "USAGE: ./powersed [filename] [occurence] [outputacement]" << std::endl;
 		return (1);
 	}
-	replacefile(av[1], std::string(av[1]).append(".replace"), av[2], av[3]);
+	outputacefile(av[1], std::string(av[1]).append(".replace"), av[2], av[3]);
 	return (0);
 }
