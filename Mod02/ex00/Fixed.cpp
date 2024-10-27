fc0e1f8de4b4fa378273ea452fb18283a735d0a1
Gugor <hmontoyavazquez@gmail.com>
2024-10-27 19:09:46 +0100

    fixed ex02 done

---
+++ Mod02/ex00/Fixed.cpp
@@ -0,0 +1,38 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &c)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->_value = c._value;
+}
+
+Fixed &Fixed::operator=(const Fixed &c)
+{
+	std::cout << "Copy Assignment operator called" << std::endl;
+	if (this != &c)
+		this->_value = c.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
+}
+
+void Fixed::setRawBits(const int raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
+
---
+++ Mod02/ex00/Fixed.hpp
@@ -0,0 +1,23 @@
+#ifndef FLOAT_H
+#define FLOAT_H
+
+#include <string>
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int _value;
+		const static int _fractions = 8;
+
+	public:
+		Fixed(void);
+		Fixed(const Fixed &c);
+		Fixed  &operator=(const Fixed &c);
+		~Fixed();
+		
+		int getRawBits(void) const;
+		void setRawBits(const int raw);
+};
+
+#endif
