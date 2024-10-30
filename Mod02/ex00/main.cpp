e4ca0a8a299a37557c2af568fef8da16c087d936
Gugor <hmontoyavazquez@gmail.com>
2024-10-30 14:27:34 +0100

    upsated main

--- Mod02/ex00/main.cpp
+++ Mod02/ex00/main.cpp
@@ -11,7 +11,7 @@ int main()
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
-	a.setRawBits(144);
+//	a.setRawBits(144);
 	std::cout << a.getRawBits() << std::endl;
 	return (0);
 }
