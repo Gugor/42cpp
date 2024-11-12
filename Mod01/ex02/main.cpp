1904889cd7e430b9b2f8755994d3f08bc7135f16
hmontoya <hmontoya@student.42barcelona.com>
2024-11-12 14:11:07 +0100

    ref ptr prepared to eval

--- Mod01/ex02/main.cpp
+++ Mod01/ex02/main.cpp
@@ -3,15 +3,21 @@
 
 int main(void)
 {
+	std::string msg 	= "HI THIS IS BRAIN";
 	std::string *stringPTR;
-	std::string msg 		= "HI THIS IS BRAIN";
+	stringPTR 		= &msg;
 	std::string &stringREF	= msg;
-	stringPTR 				= &msg;
-	std::cout << "string: " << &msg << std::endl;
-	std::cout << "PTR: " << &stringPTR << std::endl;
-	std::cout << "REF: " << &stringREF << std::endl;
+
+	std::cout << "string ref: " << &msg << std::endl;
+	std::cout << "string cont: " << msg << std::endl;
+
 	std::cout << std::endl;
-	std::cout << "string: " << msg << std::endl;
-	std::cout << "PTR: " << stringPTR << std::endl;
-	std::cout << "REF: " << stringREF << std::endl;
+
+	std::cout << "PTR by ref: " << &stringPTR << std::endl;
+	std::cout << "REF by ref: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "PTR by content: " << stringPTR << std::endl;
+	std::cout << "REF by content: " << stringREF << std::endl;
 }
