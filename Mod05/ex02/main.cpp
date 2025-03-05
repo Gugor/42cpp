bc8911b4c4a7d6f73ddd2159cc9b7dd4b8dac510
HugoMonVaz <hmontoyavazquez@gmail.com>
2025-03-05 13:33:24 +0100

    folder cleaned

--- Mod05/ex02/main.cpp
+++ Mod05/ex02/main.cpp
@@ -12,11 +12,12 @@ int main(void)
 	{
 		Bureaucrat An;
 		Bureaucrat En("En", 25, 5);
+		Bureaucrat Un("Un", 24, 3);
 		Bureaucrat Steve("Steve", 1, 1);
 		Bureaucrat Diskard("Diskard", 26, 5);
 		Bureaucrat SteveCpy;
 		ShrubberyCreationForm	shcf1("shcf1");
-		RobotomyRequestForm		rbtm("Steve");
+		RobotomyRequestForm		rbtm("Robert");
 		PresidentialPardonForm	ppf("Robbie");
 		PresidentialPardonForm	ppf2("Grantt");
 		SteveCpy = Steve;
@@ -29,32 +30,45 @@ int main(void)
 		}
 		catch (std::exception &e)
 		{
-			std::cerr << "X Error " << e.what() << ": not possible to create Form with such a low grade." << std::endl;	
+			std::cerr << "X Error " << e.what() << ": not possible to execute Form with such a low grade." << std::endl;	
 		}
+		std::cout << std::endl;
 		try 
 		{
 			Steve.executeForm(rbtm);
 		}
 		catch (std::exception &e)
 		{
-			std::cerr << "X Error " << e.what() << ": not possible to create Form with such a low grade." << std::endl;	
+			std::cerr << "X Error " << e.what() << ": not possible to execute Form with such a low grade." << std::endl;	
 		}
+		std::cout << std::endl;
 		try 
 		{
 			An.executeForm(ppf);
 		}
 		catch (std::exception &e)
 		{
-			std::cerr << "X Error " << e.what() << ": not possible to create Form with such a low grade." << std::endl;	
+			std::cerr << "X Error " << e.what() << ": not possible to execute Form with such a low grade." << std::endl;	
 		}
+		std::cout << std::endl;
 		try 
 		{
 			En.executeForm(ppf);
 		}
 		catch (std::exception &e)
 		{
-			std::cerr << "X Error " << e.what() << ": not possible to create Form with such a low grade." << std::endl;	
+			std::cerr << "X Error " << e.what() << ": not possible to execute Form with such a low grade." << std::endl;	
 		}
+		std::cout << std::endl;
+		try 
+		{
+			Un.executeForm(ppf);
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << "X Error " << e.what() << ": not possible to execute Form with such a low grade." << std::endl;	
+		}
+		std::cout << std::endl;
 		try 
 		{
 			Diskard.executeForm(ppf2);
@@ -63,7 +77,6 @@ int main(void)
 		{
 			std::cerr << "X Error " << e.what() << ": not possible to create Form with such a low grade." << std::endl;	
 		}
-
 		std::cout << std::endl;
 	}
 }
